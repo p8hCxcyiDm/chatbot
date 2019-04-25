53a35f689951a02e0272ae385f298c1ff342f1d2
Samuel Dällenbach <daellenbach@sowatec.com>
2019-04-25 12:29:03 +0200

    hello world

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+/**
+ * <p>calculo (c) 2019 by Sowatec AG, Switzerland.</p>
+ *
+ * @author Samuel Dällenbach
+ *
+ */
+public class Main {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("hello world");
+  }
+
+}
