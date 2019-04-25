af639892a8327434012f4ecfda7407db0b985233
kleidt <kleidt@sowatec.com>
2019-04-25 12:35:02 +0200

    push test

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,16 @@
 package main;
 
 /**
- * <p>calculo (c) 2019 by Sowatec AG, Switzerland.</p>
+ * <p>
+ * calculo (c) 2019 by Sowatec AG, Switzerland.
  *
  * @author Samuel Dällenbach
- *
  */
 public class Main {
 
   /**
    * @param args
+   *          command line arguments
    */
   public static void main(String[] args) {
     System.out.println("hello world");
